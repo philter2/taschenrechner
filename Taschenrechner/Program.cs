d3e736bd4771cc866b3c226707e3e0c704602bdb
Philipp Terhorst <philipp-terhorst@web.de>
2017-02-11 19:35:17 +0100

    User Story hinzugefügt

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -10,6 +10,24 @@ namespace Taschenrechner
     {
         static void Main(string[] args)
         {
+
+            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen
+            Console.WriteLine("Erste Zahl eingeben und mit Enter bestätigen:");
+            string ersterSummand = Console.ReadLine();
+            Console.WriteLine("Zweite Zahl eingeben und mit Enter bestätigen:");
+            string zweiterSummand = Console.ReadLine();
+
+            // Konvertierung string to float
+            double ersterSumandInt = Convert.ToDouble(ersterSummand);
+            double zweiterSummandInt = Convert.ToDouble(zweiterSummand);
+
+            // Berechnung ausführen
+            double summe = ersterSumandInt + zweiterSummandInt;
+
+
+            // Ausgabe
+            Console.WriteLine("Ergebnis: {0}", summe);
+            Console.ReadLine();
         }
     }
 }
